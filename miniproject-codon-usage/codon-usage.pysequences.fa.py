0078b1fc9b34938d607b88c780eb569ee6ed5fc7
Anirudh Seshadri <aseshad5@jh.edu>
2025-09-04 15:00:43 -0400

    Updated script for cytoplasm and membrane abundance

--- miniproject-codon-usage/codon-usage.pysequences.fa.py
+++ miniproject-codon-usage/codon-usage.pysequences.fa.py
@@ -1,11 +1,17 @@
 import sys
 import fasta
+import codons
+import codons
 
 fs = open(sys.argv[1])
+fs1 = open("codons.py")
+
 FASTAreader = fasta.FASTAReader(fs)
 
 codon = ""
 
+codon_to_aa = {}
+aas = {}
 
 for ident, sequence in FASTAreader:
     counter = 0
@@ -14,9 +20,20 @@ for ident, sequence in FASTAreader:
         if counter < len(sequence) - 1:
             codon = sequence[counter] + sequence[counter + 1] + sequence[counter + 2]
             #print(codon)
+            if codon in codons.forward:
+                codon_to_aa[codon] = codons.forward[codon]
+                key = codons.forward[codon]
+                aas.setdefault(key, 0)
+                aas[key] += 1
             counter += 3
             continue
         else:
             continue
+
+print(aas)
+
+
+
+
 
 
