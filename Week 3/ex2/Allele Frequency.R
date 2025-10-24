b38025ffdc477d8ddb681dd91717740a33891797
Anirudh Seshadri <aseshad5@jh.edu>
2025-10-24 15:52:21 -0400

    Questions 2.1 and 2.2 answered

--- Week 3/ex2/Allele Frequency.R
+++ Week 3/ex2/Allele Frequency.R
@@ -4,4 +4,6 @@ af <- read.csv("~/qb25-answers/Week 3/ex2/AF.txt", header = TRUE)
 
 hist(af$X0, breaks = 11, xlab = "Allele Frequncy", main = "Allele Frequency Distribution")
 
-# The distribution has a peak in the bin containing allele frequencies of 0, with a muted normal distribution (relatively) peaking at around 0.4. Does it look as expected...?
+# Question 2.1: Interpret this figure in two or three sentences in your own words. Does it look as expected? Why or why not? Bonus: what is the name of this distribution?
+  
+# The distribution of allele frequencies shows a strong peak around 0.5. This indicates that both alleles are present at similar frequencies across the population. This is expected under conditions of recombination and no strong selection where allele frequencies tend to drift toward equilibrium around 0.5. The shape of the curve resembles a normal distribution.
--- Week 3/ex2/Depth Distribution.R
+++ Week 3/ex2/Depth Distribution.R
@@ -1,5 +1,9 @@
 library(tidyverse)
 
-dp <- read.csv("~/qb25-answers/Week 3/BYxRM_bam/DP.txt", header = TRUE)
+dp <- read.csv("~/qb25-answers/Week 3/ex2/DP.txt", header = TRUE)
 
-hist(dp$X3, breaks = 21, xlab = "Depth Frequency", main = "Depth Frequency Distribution")
+hist(dp$X1, breaks = 21, xlab = "Depth Frequency", main = "Depth Frequency Distribution")
+
+# Question 2.2: Interpret this figure in two or three sentences in your own words. What does it represent? Does it look as expected? Why or why not? Bonus: what is the name of this distribution?
+
+# This figure shows the distribution of sequencing read depths across all S. cerevisiae samples. Most sites have moderate to high coverage which indicates reliable data, though the left-skew suggests some regions were sequenced at lower depth. The left skewed distribution is typical for sequencing experiments.
