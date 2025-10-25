e81697050e3f0f0945d5f2c1d64d61564d75ddff
Anirudh Seshadri <aseshad5@jh.edu>
2025-10-24 22:02:17 -0400

    corrections

--- Week 3/ex2/ex2.py
+++ Week 3/ex2/ex2.py
@@ -1,6 +1,7 @@
 for line in open("/Users/cmdb/qb25-answers/Week 3/biallelic.vcf"):
     if line.startswith('#'):
         continue
+    
     fields = line.rstrip('\n').split('\t')
     #column 9 and farther are samples. 4th position of :'s is depth
 
@@ -10,6 +11,13 @@ for line in open("/Users/cmdb/qb25-answers/Week 3/biallelic.vcf"):
      #   if i.startswith("AF="):
       #      print(i[3:])
 
-    for i in fields2:
-        if i.startswith("DP="):
-            print(i[3])
+    format_fields = fields[8].split(':')
+    if "DP" in format_fields:
+        dp_index = format_fields.index("DP")
+    else:
+        dp_index = None
+
+    for i in fields[9:]:
+        i = i.split(':')
+        if dp_index is not None and len(i) > dp_index:
+            print(i[dp_index])
